b5576d4bc35a2736588d3d455c0690721281157d
ipalacios4 <ivan.pala48@gmail.com>
2019-03-20 16:05:08 -0700

    first commited file

---
+++ Shop.java
@@ -0,0 +1,61 @@
+import static java.lang.System.*;
+import static java.lang.Math.*;
+import java.util.*;
+public class Shop
+{
+public static itemSetup
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public static void main (String[]args)
+  {
+  int function;
+  String [] products;
+  i
+		Scanner kb = new Scanner(in);
+    out.println("This program supports 4 functions: ");
+    out.println("1. Setup Shop");
+    out.println("2. Buy Items");
+    out.println("3. List Items");
+    out.println("4. Checkout");
+    out.print("Please choose the function you want: ");
+    function = kb.nextInt;
+  	if(function == 1)
+    {
+      out.print("Please enter the number of items to setup shop: ");
+      products = new String [kb.nextInt()-1];
+    }
+    if(function == 2)
+    {
+      out.print("Shop is not set up yet!");
+    }
+		if(function == 3)
+    {
+      out.print("Shop is not set up yet!");
+    }
+    if(function == 4)
+    {
+      out.print("Shop is not set up yet!");
+    }
+
+
+
+  }
+}
